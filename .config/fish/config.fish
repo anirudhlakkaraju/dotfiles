1037903303e5a5526a591a9aaff104a1f033605d
Anirudh Lakkaraju <anirudhlakkaraju@Anirudhs-MBP.attlocal.net>
2025-01-17 16:52:34 -0600

    feat: add Go path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 # Define config home
 export XDG_CONFIG_HOME="$HOME/.config"
 
+# Set Go path
+set -gx PATH $PATH (go env GOPATH)/bin
+
+# Set Homebrew path
 /opt/homebrew/bin/brew shellenv | source
 
 if status is-interactive
