96a2400acbf5f48ba653af9dce8aca3dab6e004e
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-01-31 10:42:08 -0600

    feat: remove fish symlink

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,6 @@
 # chmod +x ~/dotfiles/setup.sh
 # ~/dotfiles/setup.sh
 
-# Symlink XDG configs
-ln -sf ~/dotfiles/.config/fish ~/.config/fish
-
 # Symlink home directory configs
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
